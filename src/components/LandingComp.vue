1136244e1962ebb513b5fe024b5cb6e0aef91313
Chany4 <chandreleighdavids04052005@gmail.com>
2024-07-01 16:00:07 +0200

    update

--- src/components/LandingComp.vue
+++ src/components/LandingComp.vue
@@ -1,13 +1,48 @@
 <template>
     <div>
+        <!-- Landing Page -->
+      <div class="" data-aos="zoom-in-right" id="Landing" v-for="job in jobTitleData()" :key="job">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-md-8 d-flex justify-content-start">
+            <div class="home-text">
+              <h1 class="text-capitalize">{{job.title}}</h1>
+              <h3>{{ job.nameInJob }}</h3>
+              <p>
+                {{ job.jobBrief }}
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <img
+              src="https://chany4.github.io/images.Portfolio/images/self3.jpg"
+              class="w-50"
+              alt="self3"
+              id="self3"
+            />
+          </div>
+        </div>
+      </div>
         
     </div>
 </template>
 <script>
 export default {
-    
+    methods:{
+        jobTitleData(){
+            return this.$store.state.jobTitle
+    },
+  },
+    computed: {
+        getjobTitleData(){
+            return this.$store.dispatch('getData')
+    },
+},
+mounted() {
+    this.getjobTitleData
+    // console.log(this.$store.state.jobTitle)
+}
+
 }
 </script>
-<style lang="">
+<style></style>
     
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,57 @@
 import { createStore } from 'vuex'
-
+// import axios from 'axios'
 export default createStore({
   state: {
+    jobTitle: null,
+    about: null,
+    education: null,
+    workExp: null,
+    skill: null,
+    projects: null,
+    testimonials: null,
+   
   },
   getters: {
   },
   mutations: {
+    setjobTitle(state, payload) {
+      state.jobTitle = payload
+    },
+    setabout (state, payload) {
+      state.about = payload
+    },
+    seteducation (state, payload) {
+      state.education = payload
+    },
+    setworkExp (state, payload) {
+      state.workExp = payload
+    },
+    setskill (state, payload) {
+      state.skill = payload
+    },
+    setprojects (state, payload) {
+      state.projects = payload
+    },
+    settestimonials (state, payload) {
+      state.testimonials = payload
+    },
   },
   actions: {
+    async getData({commit}){
+      let fetchInfo = await fetch('https://chany4.github.io/portfolio-vue.js-API/data/data.json')
+      let data = await fetchInfo.json()
+      console.log(data);
+      let { jobTitle,about,education,workExp,skill,projects,testimonials} = data
+
+      commit('setjobTitle',jobTitle)
+      commit('setabout',about)
+      commit('seteducation',education)
+      commit('setworkExp',workExp)
+      commit('setskill',skill)
+      commit('setprojects',projects)
+      commit('settestimonials',testimonials)
+      
+    }
   },
   modules: {
   }
