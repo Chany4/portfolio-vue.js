90215b3f8b7b300c624cd34d5192caf6ae04ee77
Chany4 <chandreleighdavids04052005@gmail.com>
2024-10-16 13:10:01 +0200

    edited

--- src/components/ResumeComp.vue
+++ src/components/ResumeComp.vue
@@ -115,10 +115,6 @@
                     </ul>
                     </div>
                 </div>
-            <button type="button" class="btn Btn" data-bs-toggle="modal" data-bs-target="#proficiency">
-              <div class="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
-              <div class="text">Level</div>
-            </button>
 
               </div>
               <div class="col-sm-6">
@@ -145,38 +141,7 @@
               <div class="d-flex justify-content-around">
                 <p>{{ education.other }}</p>
               </div>
-              <div class="row">
-          <div class="col-12 d-flex justify-content-start align-content-center">
-            <a href="#skills">
-            <button class="Btn">
-              <div class="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
-              <div class="text">Skills</div>
-            </button>
-            </a>
-          </div>
-        </div>
-        <!-- Modal -->
-        <div class="modal fade" id="proficiency" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="proficiency">Proficiency</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <ul>
-          <li>English : Advanced (Home language)</li>
-          <li>Afrikaans : Advanced</li>
-          <li>Korean : Beginner</li>
-        </ul>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-            <!-- End of modal -->
+        
             </div>
             
           </div>
